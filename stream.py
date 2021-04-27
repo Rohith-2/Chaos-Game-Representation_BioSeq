8d54b2370adaaee0610874e864c0adee0baf6f3e
rohith <rrohith2001@gmail.com>
2021-04-27 22:08:45 +0530

    changed location of py

--- stream.py
+++ stream.py
@@ -16,6 +16,7 @@ from skimage.metrics import structural_similarity as ssim
 import matplotlib 
 matplotlib.use("Agg") 
 st.set_option('deprecation.showPyplotGlobalUse', False)
+
 l = os.getcwd()
 if(l!='/app/chaos-game-representation_bioseq/data/'):
      os.chdir('/app/chaos-game-representation_bioseq/data/')
