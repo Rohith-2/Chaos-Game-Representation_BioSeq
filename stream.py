0ee212c44a4a35e3019eebd24f5cffd2b29cdc5e
Rohith Ramakrishnan <55501708+Rohith-2@users.noreply.github.com>
2021-05-07 17:51:58 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -111,6 +111,7 @@ class CGR():
             maxy = array_size
             posx = 1
             posy = 1
+        chaos = chaos/np.amax(chaos)
         return chaos
 
     def load_fasta(self,data,head):
@@ -128,7 +129,7 @@ class CGR():
     def show(self):
         pylab.figure(figsize=(12,12))
         pylab.title('CGR of '+str(self.K)+'-mers for '+self.h[2:])
-        pylab.imshow(self.c, cmap=cm.gray_r)
+        pylab.imshow(self.c, cmap=cm.gray_r,filterrad=10,vmin=0,vmax=2)
         ax = pylab.gca()
         ax.axes.xaxis.set_visible(False)
         ax.axes.yaxis.set_visible(False)
